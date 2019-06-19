8d70b85c08fdcc518f1cf582ac753c1960d8a282
stevescorfield <steve.scorfield@ons.gov.uk>
2019-06-19 12:03:42 +0100

    Initial creation of code for this project

---
+++ src/main/java/uk/gov/ons/census/fwmt/csvserivce/CsvserivceApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.ons.census.fwmt.csvserivce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvserivceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvserivceApplication.class, args);
+	}
+
+}
