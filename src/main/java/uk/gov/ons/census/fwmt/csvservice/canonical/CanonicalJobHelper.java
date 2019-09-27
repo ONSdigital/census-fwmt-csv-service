b59b85f0ed0515659266dbfdcf688c91caac4687
jacobharrison <jacob.harrison@qa.com>
2019-09-27 14:57:26 +0100

    formatting

--- src/main/java/uk/gov/ons/census/fwmt/csvservice/canonical/CanonicalJobHelper.java
+++ src/main/java/uk/gov/ons/census/fwmt/csvservice/canonical/CanonicalJobHelper.java
@@ -62,8 +62,6 @@ public final class CanonicalJobHelper {
     createJobRequest.setActionType("Create");
     createJobRequest.setCaseId(createCaseId);
     createJobRequest.setCaseReference(createCaseId.toString());
-    // Case type needs to be changed to below after tests are completed
-    // createJobRequest.setCaseType("CCS PL");
     createJobRequest.setCaseType("CCSPL");
     createJobRequest.setCoordinatorId(csvRecordDTO.getCoordinatorId());
     createJobRequest.setMandatoryResource(csvRecordDTO.getCcsInterviewer());
