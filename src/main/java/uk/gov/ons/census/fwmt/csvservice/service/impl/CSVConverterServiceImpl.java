a5787ce6901784067022ca897f51ad77ca11dd15
SteveScorfield <steve.scorfield@ons.gov.uk>
2019-11-29 11:59:46 +0000

    Merge branch 'master' of /Users/scorfs/Documents/Dev/census-fwmt-csv-service with conflicts.

--- src/main/java/uk/gov/ons/census/fwmt/csvservice/service/impl/CSVConverterServiceImpl.java
+++ src/main/java/uk/gov/ons/census/fwmt/csvservice/service/impl/CSVConverterServiceImpl.java
@@ -1,38 +1,42 @@
 package uk.gov.ons.census.fwmt.csvservice.service.impl;
 
-import static uk.gov.ons.census.fwmt.csvservice.config.GatewayEventsConfig.CSV_CCS_REQUEST_EXTRACTED;
-import static uk.gov.ons.census.fwmt.csvservice.config.GatewayEventsConfig.CSV_CE_REQUEST_EXTRACTED;
-
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
-
-import com.opencsv.bean.CsvToBean;
-import com.opencsv.bean.CsvToBeanBuilder;
-
-import lombok.extern.slf4j.Slf4j;
 import uk.gov.ons.census.fwmt.canonical.v1.CreateFieldWorkerJobRequest;
 import uk.gov.ons.census.fwmt.common.error.GatewayException;
 import uk.gov.ons.census.fwmt.csvservice.canonical.CanonicalJobHelper;
 import uk.gov.ons.census.fwmt.csvservice.config.GatewayEventsConfig;
-import uk.gov.ons.census.fwmt.csvservice.dto.CSVRecordDTO;
+import uk.gov.ons.census.fwmt.csvservice.dto.CCSPropertyListing;
+import uk.gov.ons.census.fwmt.csvservice.dto.CEJobListing;
 import uk.gov.ons.census.fwmt.csvservice.service.CSVAdapterService;
 import uk.gov.ons.census.fwmt.csvservice.service.CSVConverterService;
 import uk.gov.ons.census.fwmt.events.component.GatewayEventManager;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+
+import static uk.gov.ons.census.fwmt.csvservice.config.GatewayEventsConfig.CANONICAL_CCS_CREATE_SENT;
+import static uk.gov.ons.census.fwmt.csvservice.config.GatewayEventsConfig.CSV_CCS_REQUEST_EXTRACTED;
+import static uk.gov.ons.census.fwmt.csvservice.utils.CsvServiceUtils.moveCcsFile;
+
 @Slf4j
-@Service
-public class CSVConverterServiceImpl implements CSVConverterService {
-  @Value("${gcpBucket.celocation}")
-  private Resource cePath;
+@Service("CE")
+public class CCSConverterServiceImpl implements CSVConverterService {
+  @Value("${gcpBucket.ccslocation}")
+  private Resource csvGCPFile;
 
   @Value("${gcpBucket.ccslocation}")
-  private Resource ccsPath;
+  private Path csvPath;
+
+  @Value("${gcpBucket.ccsProcessedPath}")
+  private Path processedPath;
 
   @Autowired
   private CSVAdapterService csvAdapterService;
@@ -40,42 +44,26 @@ public class CSVConverterServiceImpl implements CSVConverterService {
   @Autowired
   private GatewayEventManager gatewayEventManager;
 
-  public void convertCSVToCanonical(String ingestType) throws GatewayException {
-
-    CanonicalJobHelper canonicalJobHelper;
-    CsvToBean<CSVRecordDTO> csvToBean;
-    Resource csvGCPFile;
-
-    if (ingestType.equals("CEIngest")) {
-      csvGCPFile = cePath;
-
-    } else {
-      csvGCPFile = ccsPath;
-    }
-
+  @Override
+  public void convertCSVToCanonical() throws GatewayException {
+    CsvToBean<CCSPropertyListing> csvToBean;
     try {
-      csvToBean = new CsvToBeanBuilder(
-          new InputStreamReader(csvGCPFile.getInputStream(), StandardCharsets.UTF_8))
-          .withType(CSVRecordDTO.class)
+      csvToBean = new CsvToBeanBuilder(new InputStreamReader(csvGCPFile.getInputStream(), StandardCharsets.UTF_8))
+          .withType(CCSPropertyListing.class)
           .build();
+
     } catch (IOException e) {
       String msg = "Failed to convert CSV to Bean.";
       gatewayEventManager.triggerErrorEvent(this.getClass(), msg, "N/A", GatewayEventsConfig.UNABLE_TO_READ_CSV);
       throw new GatewayException(GatewayException.Fault.SYSTEM_ERROR, e, msg);
     }
 
-    if (ingestType.equals(CSVConverterService.CE_INGEST)) {
-      for (CSVRecordDTO csvRecordDTO : csvToBean) {
-        csvAdapterService.sendJobRequest(CanonicalJobHelper.createCEJob(csvRecordDTO));
-        gatewayEventManager.triggerEvent(String.valueOf(csvRecordDTO.getCaseId()), CSV_CE_REQUEST_EXTRACTED);
-      }
-
-    } else {
-      for (CSVRecordDTO csvRecordDTO : csvToBean) {
-        CreateFieldWorkerJobRequest createCCSRequest = CanonicalJobHelper.createCCSJob(csvRecordDTO);
-        csvAdapterService.sendJobRequest(createCCSRequest);
-        gatewayEventManager.triggerEvent(String.valueOf(createCCSRequest.getCaseId()), CSV_CCS_REQUEST_EXTRACTED);
-      }
+    for (CCSPropertyListing ccsPropertyListing : csvToBean) {
+      CreateFieldWorkerJobRequest createFieldWorkerJobRequest = CanonicalJobHelper.createCCSJob(ccsPropertyListing);
+      csvAdapterService.sendJobRequest(createFieldWorkerJobRequest, CANONICAL_CCS_CREATE_SENT);
+      gatewayEventManager
+          .triggerEvent(String.valueOf(createFieldWorkerJobRequest.getCaseId()), CSV_CCS_REQUEST_EXTRACTED);
     }
+    moveCcsFile(csvGCPFile, csvPath, processedPath);
   }
 }
---
+++ src/test/java/uk/gov/ons/fwmt/csvservice/service/CCSConverterServiceImplTest.java
@@ -0,0 +1,61 @@
+package uk.gov.ons.fwmt.csvservice.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.WritableResource;
+import org.springframework.test.util.ReflectionTestUtils;
+import uk.gov.ons.census.fwmt.common.error.GatewayException;
+import uk.gov.ons.census.fwmt.csvservice.service.impl.CCSConverterServiceImpl;
+import uk.gov.ons.census.fwmt.csvservice.service.impl.CSVAdapterServiceImpl;
+import uk.gov.ons.census.fwmt.events.component.GatewayEventManager;
+
+import java.io.OutputStream;
+import java.nio.file.Path;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static uk.gov.ons.census.fwmt.csvservice.config.GatewayEventsConfig.CSV_CCS_REQUEST_EXTRACTED;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CCSConverterServiceImplTest {
+
+  @InjectMocks
+  private CCSConverterServiceImpl ccsConverterService;
+
+  @Mock
+  private CSVAdapterServiceImpl csvAdapterService;
+
+  @Mock
+  private GatewayEventManager gatewayEventManager;
+
+  @Mock
+  private OutputStream outputStream;
+
+  @Mock
+  private WritableResource writableResource;
+
+  @Test
+  public void convertCCSCSVToCanonicalTest() throws GatewayException {
+    // Given
+    ClassLoader classLoader = getClass().getClassLoader();
+    String testPathString = classLoader.getResource("testCCSCSV.csv").getPath();
+    Path testPath = Path.of("/", testPathString);
+    FileSystemResource testResource = new FileSystemResource(testPathString);
+
+    ReflectionTestUtils.setField(ccsConverterService, "csvGCPFile", testResource);
+    ReflectionTestUtils.setField(ccsConverterService, "csvPath", testPath);
+    ReflectionTestUtils.setField(ccsConverterService, "processedPath", testPath);
+
+    // When
+    ccsConverterService.convertCSVToCanonical();
+
+    // Then
+    Mockito.verify(gatewayEventManager).triggerEvent(anyString(), eq(CSV_CCS_REQUEST_EXTRACTED));
+  }
+}
