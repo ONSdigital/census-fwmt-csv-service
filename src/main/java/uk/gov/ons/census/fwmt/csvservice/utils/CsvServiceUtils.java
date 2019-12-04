0a791c8a6e421177943331e2863aa162fe0d6971
jacobharrison <jacob.harrison@qa.com>
2019-12-04 08:13:32 +0000

    after file is processed it is moved to a processed folder and renamed with processed-timestamp.csv

--- src/main/java/uk/gov/ons/census/fwmt/csvservice/utils/CsvServiceUtils.java
+++ src/main/java/uk/gov/ons/census/fwmt/csvservice/utils/CsvServiceUtils.java
@@ -6,19 +6,19 @@ import uk.gov.ons.census.fwmt.common.error.GatewayException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.sql.Timestamp;
-import java.time.Instant;
 
 public final class CsvServiceUtils {
 
   public static void moveCsvFile(Resource csvGCPFile, Path csvPath, Path processedPath) throws GatewayException {
     Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-    Instant instant = timestamp.toInstant();
+    String timeStamp = String.valueOf(timestamp.toInstant());
     String originalFileName = csvGCPFile.getFilename();
 
     if (originalFileName != null) {
       try {
-        Files.move(csvPath, processedPath);
+        Files.move(csvPath, processedPath.resolveSibling("processed-" + timeStamp + ".csv"), StandardCopyOption.REPLACE_EXISTING);
       } catch (IOException e) {
         throw new GatewayException(GatewayException.Fault.SYSTEM_ERROR, e, "Unable to move ingest CSV");
       }
