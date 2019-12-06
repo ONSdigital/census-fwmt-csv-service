2d12792d7cb38ef3dce58de5104ba670442faee4
jacobharrison <jacob.harrison@qa.com>
2019-12-06 14:51:03 +0000

    i am blind

--- src/main/java/uk/gov/ons/census/fwmt/csvservice/controller/CsvMonitorController.java
+++ src/main/java/uk/gov/ons/census/fwmt/csvservice/controller/CsvMonitorController.java
@@ -5,26 +5,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import uk.gov.ons.census.fwmt.common.error.GatewayException;
-import uk.gov.ons.census.fwmt.csvservice.implementation.ccs.CCSConverterService;
-import uk.gov.ons.census.fwmt.csvservice.implementation.ce.CEConverterService;
+import uk.gov.ons.census.fwmt.csvservice.service.CSVConverterService;
+
+import java.util.Map;
 
 @Controller
 public class CsvMonitorController {
 
   @Autowired
-  private CEConverterService ceConverterService;
-
-  @Autowired
-  private CCSConverterService ccsConverterService;
+  private Map<String, CSVConverterService> serviceMap;
 
   @GetMapping("/ingestCeCsvFile")
   public ResponseEntity<String> ingestCeCsvFile() throws GatewayException {
+    final CSVConverterService ceConverterService = serviceMap.get("CE");
     ceConverterService.convertToCanonical();
     return ResponseEntity.ok("CSV adapter service activated");
   }
 
   @GetMapping("/ingestCCSCsvFile")
   public ResponseEntity<String> ingestCSSCsvFile() throws GatewayException {
+    final CSVConverterService ccsConverterService = serviceMap.get("CCS");
     ccsConverterService.convertToCanonical();
     return ResponseEntity.ok("CSV adapter service activated");
   }
