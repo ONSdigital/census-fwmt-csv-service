6dcc453cf11829935ac470a0b37d03ada3870bec
jacobharrison <jacob.harrison@qa.com>
2019-12-09 15:49:16 +0000

    remove tests

--- src/test/java/uk/gov/ons/fwmt/csvservice/implementation/ccs/CCSConverterServiceImplTest.java
+++ src/test/java/uk/gov/ons/fwmt/csvservice/implementation/ccs/CCSConverterServiceImplTest.java
@@ -1,45 +1,45 @@
-package uk.gov.ons.fwmt.csvservice.implementation.ccs;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.WritableResource;
-import org.springframework.test.util.ReflectionTestUtils;
-import uk.gov.ons.census.fwmt.common.error.GatewayException;
-import uk.gov.ons.census.fwmt.csvservice.adapter.GatewayActionAdapter;
-import uk.gov.ons.census.fwmt.csvservice.implementation.ccs.CCSConverterService;
-import uk.gov.ons.census.fwmt.events.component.GatewayEventManager;
-
-import java.io.OutputStream;
-import java.nio.file.Path;
-
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
-import static uk.gov.ons.census.fwmt.csvservice.implementation.ccs.CCSGatewayEventsConfig.CSV_CCS_REQUEST_EXTRACTED;
-
-@RunWith(MockitoJUnitRunner.class)
-public class CCSConverterServiceImplTest {
-
-  @InjectMocks
-  private CCSConverterService ccsConverterService;
-
-  @Mock
-  private GatewayActionAdapter gatewayActionAdapter;
-
-  @Mock
-  private GatewayEventManager gatewayEventManager;
-
-  @Mock
-  private OutputStream outputStream;
-
-  @Mock
-  private WritableResource writableResource;
-
+//package uk.gov.ons.fwmt.csvservice.implementation.ccs;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.Mockito;
+//import org.mockito.junit.MockitoJUnitRunner;
+//import org.springframework.core.io.FileSystemResource;
+//import org.springframework.core.io.Resource;
+//import org.springframework.core.io.WritableResource;
+//import org.springframework.test.util.ReflectionTestUtils;
+//import uk.gov.ons.census.fwmt.common.error.GatewayException;
+//import uk.gov.ons.census.fwmt.csvservice.adapter.GatewayActionAdapter;
+//import uk.gov.ons.census.fwmt.csvservice.implementation.ccs.CCSConverterService;
+//import uk.gov.ons.census.fwmt.events.component.GatewayEventManager;
+//
+//import java.io.OutputStream;
+//import java.nio.file.Path;
+//
+//import static org.mockito.ArgumentMatchers.anyString;
+//import static org.mockito.ArgumentMatchers.eq;
+//import static uk.gov.ons.census.fwmt.csvservice.implementation.ccs.CCSGatewayEventsConfig.CSV_CCS_REQUEST_EXTRACTED;
+//
+//@RunWith(MockitoJUnitRunner.class)
+//public class CCSConverterServiceImplTest {
+//
+//  @InjectMocks
+//  private CCSConverterService ccsConverterService;
+//
+//  @Mock
+//  private GatewayActionAdapter gatewayActionAdapter;
+//
+//  @Mock
+//  private GatewayEventManager gatewayEventManager;
+//
+//  @Mock
+//  private OutputStream outputStream;
+//
+//  @Mock
+//  private WritableResource writableResource;
+//
 //  @Test
 //  public void convertCCSCSVToCanonicalTest() throws GatewayException {
 //    // Given
@@ -58,4 +58,4 @@ public class CCSConverterServiceImplTest {
 //    // Then
 //    Mockito.verify(gatewayEventManager).triggerEvent(anyString(), eq(CSV_CCS_REQUEST_EXTRACTED));
 //  }
-}
+//}
--- src/test/java/uk/gov/ons/fwmt/csvservice/implementation/ce/CEConverterServiceImplTest.java
+++ src/test/java/uk/gov/ons/fwmt/csvservice/implementation/ce/CEConverterServiceImplTest.java
@@ -1,45 +1,45 @@
-package uk.gov.ons.fwmt.csvservice.implementation.ce;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.WritableResource;
-import org.springframework.test.util.ReflectionTestUtils;
-import uk.gov.ons.census.fwmt.common.error.GatewayException;
-import uk.gov.ons.census.fwmt.csvservice.adapter.GatewayActionAdapter;
-import uk.gov.ons.census.fwmt.csvservice.implementation.ce.CEConverterService;
-import uk.gov.ons.census.fwmt.events.component.GatewayEventManager;
-
-import java.io.OutputStream;
-import java.nio.file.Path;
-
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
-import static uk.gov.ons.census.fwmt.csvservice.implementation.ce.CEGatewayEventsConfig.CSV_CE_REQUEST_EXTRACTED;
-
-@RunWith(MockitoJUnitRunner.class)
-public class CEConverterServiceImplTest {
-
-  @InjectMocks
-  private CEConverterService ceConverterService;
-
-  @Mock
-  private GatewayActionAdapter gatewayActionAdapter;
-
-  @Mock
-  private GatewayEventManager gatewayEventManager;
-
-  @Mock
-  private OutputStream outputStream;
-
-  @Mock
-  private WritableResource writableResource;
-
+//package uk.gov.ons.fwmt.csvservice.implementation.ce;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.Mockito;
+//import org.mockito.junit.MockitoJUnitRunner;
+//import org.springframework.core.io.FileSystemResource;
+//import org.springframework.core.io.Resource;
+//import org.springframework.core.io.WritableResource;
+//import org.springframework.test.util.ReflectionTestUtils;
+//import uk.gov.ons.census.fwmt.common.error.GatewayException;
+//import uk.gov.ons.census.fwmt.csvservice.adapter.GatewayActionAdapter;
+//import uk.gov.ons.census.fwmt.csvservice.implementation.ce.CEConverterService;
+//import uk.gov.ons.census.fwmt.events.component.GatewayEventManager;
+//
+//import java.io.OutputStream;
+//import java.nio.file.Path;
+//
+//import static org.mockito.ArgumentMatchers.anyString;
+//import static org.mockito.ArgumentMatchers.eq;
+//import static uk.gov.ons.census.fwmt.csvservice.implementation.ce.CEGatewayEventsConfig.CSV_CE_REQUEST_EXTRACTED;
+//
+//@RunWith(MockitoJUnitRunner.class)
+//public class CEConverterServiceImplTest {
+//
+//  @InjectMocks
+//  private CEConverterService ceConverterService;
+//
+//  @Mock
+//  private GatewayActionAdapter gatewayActionAdapter;
+//
+//  @Mock
+//  private GatewayEventManager gatewayEventManager;
+//
+//  @Mock
+//  private OutputStream outputStream;
+//
+//  @Mock
+//  private WritableResource writableResource;
+//
 //  @Test
 //  public void convertCECSVToCanonicalTest() throws GatewayException {
 //    // Given
@@ -58,4 +58,4 @@ public class CEConverterServiceImplTest {
 //    // Then
 //    Mockito.verify(gatewayEventManager).triggerEvent(anyString(), eq(CSV_CE_REQUEST_EXTRACTED));
 //  }
-}
+//}
