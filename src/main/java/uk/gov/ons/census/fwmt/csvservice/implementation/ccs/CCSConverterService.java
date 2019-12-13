5ae3143cd6abc2ff86532a81c327dcf93b9d49ff
Simon <sdiaz1000@gmail.com>
2019-12-13 15:03:49 +0000

    use-streamutils-to-copy

--- src/main/java/uk/gov/ons/census/fwmt/csvservice/implementation/ccs/CCSConverterService.java
+++ src/main/java/uk/gov/ons/census/fwmt/csvservice/implementation/ccs/CCSConverterService.java
@@ -4,8 +4,12 @@ import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.WritableResource;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StreamUtils;
+
 import uk.gov.ons.census.fwmt.canonical.v1.CreateFieldWorkerJobRequest;
 import uk.gov.ons.census.fwmt.common.error.GatewayException;
 import uk.gov.ons.census.fwmt.csvservice.adapter.GatewayActionAdapter;
@@ -16,13 +20,15 @@ import uk.gov.ons.census.fwmt.events.component.GatewayEventManager;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.sql.Timestamp;
 
 import static uk.gov.ons.census.fwmt.csvservice.implementation.ccs.CCSCanonicalBuilder.createCCSJob;
 import static uk.gov.ons.census.fwmt.csvservice.implementation.ccs.CCSGatewayEventsConfig.CANONICAL_CCS_CREATE_SENT;
 import static uk.gov.ons.census.fwmt.csvservice.implementation.ccs.CCSGatewayEventsConfig.CSV_CCS_REQUEST_EXTRACTED;
-import static uk.gov.ons.census.fwmt.csvservice.utils.CsvServiceUtils.moveCsvFile;
 
 @Component("CCS")
 public class CCSConverterService implements CSVConverterService {
@@ -38,9 +44,16 @@ public class CCSConverterService implements CSVConverterService {
 
   @Autowired
   private GatewayEventManager gatewayEventManager;
+  
+  @Autowired
+  private DefaultResourceLoader defaultResourceLoader;
 
   @Override
   public void convertToCanonical() throws GatewayException {
+    Resource fileR = defaultResourceLoader.getResource("file://csv-bucket-test/ccsTestCSV.csv");
+    Resource gR = defaultResourceLoader.getResource("gs://csv-bucket-test/processed/ccsTestCSV.csv");
+    
+    
     CsvToBean<CCSPropertyListing> csvToBean;
     try {
       csvToBean = new CsvToBeanBuilder(new InputStreamReader(csvGCPFile.getInputStream(), StandardCharsets.UTF_8))
@@ -61,9 +74,41 @@ public class CCSConverterService implements CSVConverterService {
     }
 
     try {
-      moveCsvFile(csvGCPFile, Paths.get(csvGCPFile.getURI()), Paths.get(processedPath.getURI()));
+      moveCsvFile(csvGCPFile, Paths.get(csvGCPFile.getURI()), processedPath);
+      //moveCsvFile(csvGCPFile, processedPath);
     } catch (IOException e) {
       throw new GatewayException(GatewayException.Fault.SYSTEM_ERROR, e, "Failed to read path");
     }
   }
+
+  public void moveCsvFile(Resource csvGCPFile, Path csvPath, Resource processedPath) throws GatewayException {
+    Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+    String timeStamp = String.valueOf(timestamp.toInstant());
+    String originalFileName = csvGCPFile.getFilename();
+
+    String outPath = "";
+    try {
+      outPath = processedPath.getURI().toString()+ originalFileName+ "processed-" + timeStamp + ".csv";
+    } catch (IOException e1) {
+      // TODO Auto-generated catch block
+      e1.printStackTrace();
+    }
+    
+    Resource outResource = defaultResourceLoader.getResource(outPath);
+
+    if (originalFileName != null) {
+
+      try (OutputStream os = ((WritableResource) outResource).getOutputStream()) {
+
+        StreamUtils.copy(csvGCPFile.getInputStream(), os);
+
+        // Files.move(csvPath, processedPath.resolveSibling("processed-" +
+        // timeStamp + ".csv"), StandardCopyOption.REPLACE_EXISTING);
+      } catch (IOException e) {
+        throw new GatewayException(GatewayException.Fault.SYSTEM_ERROR, e, "Unable to move/rename ingest CSV");
+      }
+    }
+  }
+
+
 }
--- src/main/java/uk/gov/ons/census/fwmt/csvservice/utils/CsvServiceUtils.java
+++ src/main/java/uk/gov/ons/census/fwmt/csvservice/utils/CsvServiceUtils.java
@@ -1,14 +1,18 @@
 package uk.gov.ons.census.fwmt.csvservice.utils;
 
-import org.springframework.core.io.Resource;
-import uk.gov.ons.census.fwmt.common.error.GatewayException;
-
 import java.io.IOException;
-import java.nio.file.Files;
+import java.io.OutputStream;
 import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
 import java.sql.Timestamp;
 
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.WritableResource;
+import org.springframework.util.StreamUtils;
+
+import lombok.extern.slf4j.Slf4j;
+import uk.gov.ons.census.fwmt.common.error.GatewayException;
+
+@Slf4j
 public final class CsvServiceUtils {
 
   public static void moveCsvFile(Resource csvGCPFile, Path csvPath, Path processedPath) throws GatewayException {
@@ -16,12 +20,27 @@ public final class CsvServiceUtils {
     String timeStamp = String.valueOf(timestamp.toInstant());
     String originalFileName = csvGCPFile.getFilename();
 
+    Path resolveSibling = processedPath.resolveSibling("processed-" + timeStamp + ".csv");
+
     if (originalFileName != null) {
-      try {
-        Files.move(csvPath, processedPath.resolveSibling("processed-" + timeStamp + ".csv"), StandardCopyOption.REPLACE_EXISTING);
+
+      try (OutputStream os = ((WritableResource) resolveSibling).getOutputStream()) {
+
+        StreamUtils.copy(csvGCPFile.getInputStream(), os);
+
+        // Files.move(csvPath, processedPath.resolveSibling("processed-" +
+        // timeStamp + ".csv"), StandardCopyOption.REPLACE_EXISTING);
       } catch (IOException e) {
         throw new GatewayException(GatewayException.Fault.SYSTEM_ERROR, e, "Unable to move/rename ingest CSV");
       }
+    }
+  }
+
+  public static void moveCsvFile(Resource csvGCPFile2, Resource processedPath2) throws  IOException {
+    log.info(processedPath2.getFilename());
+    try (OutputStream os = ((WritableResource) processedPath2).getOutputStream()) {
+
+      StreamUtils.copy(csvGCPFile2.getInputStream(), os);
     }
   }
 }
