5fd41418da6214688e08543fa1d7896673816f9c
jacobharrison <jacob.harrison@qa.com>
2019-12-16 15:54:50 +0000

    use blobs to rename and move

--- src/main/java/uk/gov/ons/census/fwmt/csvservice/utils/CsvServiceUtils.java
+++ src/main/java/uk/gov/ons/census/fwmt/csvservice/utils/CsvServiceUtils.java
@@ -6,6 +6,11 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.sql.Timestamp;
 
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.CopyWriter;
+import com.google.cloud.storage.Storage;
+import org.checkerframework.checker.units.qual.A;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.Resource;
@@ -21,34 +26,41 @@ import uk.gov.ons.census.fwmt.common.error.GatewayException;
 public class CsvServiceUtils {
 
   @Autowired
-  private DefaultResourceLoader defaultResourceLoader;
+  private Storage gcs;
+
+  private static final String SEPARATOR = "/";
 
   public void moveCsvFile(Resource csvGCPFile, Resource processedPath) throws GatewayException {
     Timestamp timestamp = new Timestamp(System.currentTimeMillis());
     String timeStamp = String.valueOf(timestamp.toInstant());
     String originalFileName = csvGCPFile.getFilename();
 
-    String outputPath = "Failed to read path{} ingest CSV";
+    String outputPath = null;
     try {
       outputPath = processedPath.getURI().toString() + originalFileName + "processed-" + timeStamp + ".csv";
     } catch (IOException e) {
-      throw new GatewayException(GatewayException.Fault.SYSTEM_ERROR, e, outputPath, processedPath);
+      e.printStackTrace();
     }
 
-    Resource outResource = defaultResourceLoader.getResource(outputPath);
-
-    if (originalFileName != null) {
-      try (OutputStream os = ((WritableResource) outResource).getOutputStream()) {
-        StreamUtils.copy(csvGCPFile.getInputStream(), os);
-      } catch (IOException e) {
-        throw new GatewayException(GatewayException.Fault.SYSTEM_ERROR, e, "Failed to copy ingest CSV");
-      }
-
-      try {
-        Files.delete(Path.of(csvGCPFile.getURI()));
-      } catch (IOException e) {
-        throw new GatewayException(GatewayException.Fault.SYSTEM_ERROR, e, "Failed to delete processed ingest CSV");
-      }
+    String[] fromTokens = new String[0];
+    try {
+      fromTokens = getBucketAndObjectFromPath(String.valueOf(csvGCPFile.getURI()));
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+    assert outputPath != null;
+    String[] toTokens = getBucketAndObjectFromPath(outputPath);
+
+    // There is currently no way to rename/move things in GCS, so we'll have to copy and remove.
+    BlobId source = BlobId.of(fromTokens[0], fromTokens[1]);
+    BlobId target = BlobId.of(toTokens[0], toTokens[1]);
+    Storage.CopyRequest copyRequest = Storage.CopyRequest.of(source, target);
+    this.gcs.copy(copyRequest);
+    this.gcs.delete(source);
   }
+
+  private String[] getBucketAndObjectFromPath(String path) {
+    return path.split(SEPARATOR, 2);
+  }
+
 }
