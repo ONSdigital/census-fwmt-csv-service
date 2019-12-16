cff7fe8ffd9f8cef64a25fe81acc0e985d97bad7
jacobharrison <jacob.harrison@qa.com>
2019-12-16 14:36:09 +0000

    clean

--- src/main/java/uk/gov/ons/census/fwmt/csvservice/implementation/ce/CEConverterService.java
+++ src/main/java/uk/gov/ons/census/fwmt/csvservice/implementation/ce/CEConverterService.java
@@ -18,7 +18,6 @@ import uk.gov.ons.census.fwmt.events.component.GatewayEventManager;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Paths;
 
 import static uk.gov.ons.census.fwmt.csvservice.implementation.ce.CECanonicalBuilder.createCEJob;
 import static uk.gov.ons.census.fwmt.csvservice.implementation.ce.CEGatewayEventsConfig.CANONICAL_CE_CREATE_SENT;
