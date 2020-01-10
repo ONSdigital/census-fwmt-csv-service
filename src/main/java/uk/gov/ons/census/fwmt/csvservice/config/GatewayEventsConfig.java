bf84e2e0241be2753fefdabdd0add0f8e6fdad5d
jacobharrison <jacob.harrison@qa.com>
2020-01-10 12:11:03 +0000

    add events for lookup

--- src/main/java/uk/gov/ons/census/fwmt/csvservice/config/GatewayEventsConfig.java
+++ src/main/java/uk/gov/ons/census/fwmt/csvservice/config/GatewayEventsConfig.java
@@ -12,14 +12,13 @@ public class GatewayEventsConfig {
   public static final String FAILED_TO_MARSHALL_CANONICAL = "FAILED_TO_MARSHALL_CANONICAL";
   public static final String FAILED_MATCH_POSTCODE = "FAILED_MATCH_POSTCODE";
   public static final String POSTCODE_MAP_EMPTY = "POSTCODE_MAP_EMPTY";
-  public static final String POSTCODE_LOOKUP_LOADED = "POSTCODE_LOOKUP_LOADED";
 
   @Bean
   public GatewayEventManager gatewayEventManager() {
     GatewayEventManager gatewayEventManager = new GatewayEventManager();
     gatewayEventManager.setSource(Application.APPLICATION_NAME);
     gatewayEventManager.addErrorEventTypes(new String[] {UNABLE_TO_READ_CSV, FAILED_TO_MARSHALL_CANONICAL,
-        FAILED_MATCH_POSTCODE, POSTCODE_MAP_EMPTY, POSTCODE_LOOKUP_LOADED});
+        FAILED_MATCH_POSTCODE, POSTCODE_MAP_EMPTY});
     return gatewayEventManager;
   }
 }
---
+++ src/main/java/uk/gov/ons/census/fwmt/csvservice/implementation/postcodeloader/LookupFileLoaderEventsConfig.java
@@ -0,0 +1,17 @@
+package uk.gov.ons.census.fwmt.csvservice.implementation.postcodeloader;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import uk.gov.ons.census.fwmt.events.component.GatewayEventManager;
+
+@Configuration
+public class LookupFileLoaderEventsConfig {
+
+  public static final String POSTCODE_LOOKUP_LOADED = "POSTCODE_LOOKUP_LOADED";
+
+  @Bean
+  public GatewayEventManager addAddressCheckEvents(GatewayEventManager gatewayEventManager) {
+    gatewayEventManager.addEventTypes(new String[] {POSTCODE_LOOKUP_LOADED});
+    return gatewayEventManager;
+  }
+}
