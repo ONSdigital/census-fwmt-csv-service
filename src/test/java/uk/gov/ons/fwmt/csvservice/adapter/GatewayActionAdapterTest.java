bc25565160a11351b3b1b330327c7ba97b71956e
Simon <sdiaz1000@gmail.com>
2020-10-20 11:17:22 +0100

    remove old gateway action qs

--- src/test/java/uk/gov/ons/fwmt/csvservice/adapter/GatewayActionAdapterTest.java
+++ src/test/java/uk/gov/ons/fwmt/csvservice/adapter/GatewayActionAdapterTest.java
@@ -1,44 +1,27 @@
 package uk.gov.ons.fwmt.csvservice.adapter;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
-import uk.gov.ons.census.fwmt.canonical.v1.CreateFieldWorkerJobRequest;
-import uk.gov.ons.census.fwmt.common.error.GatewayException;
-import uk.gov.ons.census.fwmt.csvservice.adapter.GatewayActionAdapter;
-import uk.gov.ons.census.fwmt.csvservice.message.GatewayActionProducer;
-import uk.gov.ons.census.fwmt.events.component.GatewayEventManager;
-import uk.gov.ons.fwmt.csvservice.helper.FieldWorkerRequestMessageBuilder;
-
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
-import static uk.gov.ons.census.fwmt.csvservice.implementation.ce.CEGatewayEventsConfig.CANONICAL_CE_CREATE_SENT;
-
-@RunWith(MockitoJUnitRunner.class)
+//@RunWith(MockitoJUnitRunner.class)
 public class GatewayActionAdapterTest {
 
-  @InjectMocks
-  private GatewayActionAdapter gatewayActionAdapter;
+//  @InjectMocks
+//  private GatewayActionAdapter gatewayActionAdapter;
 
-  @Mock
-  private GatewayEventManager gatewayEventManager;
-
-  @Mock
-  private GatewayActionProducer gatewayActionProducer;
-
-  @Test
-  public void sendCERequestToJobService() throws GatewayException {
-    // Given
-    CreateFieldWorkerJobRequest createJobRequest = new FieldWorkerRequestMessageBuilder()
-        .buildCreateFieldWorkerJobRequestCE();
-
-    // When
-    gatewayActionAdapter.sendJobRequest(createJobRequest, CANONICAL_CE_CREATE_SENT);
-
-    // Then
-    Mockito.verify(gatewayEventManager).triggerEvent(anyString(), eq(CANONICAL_CE_CREATE_SENT));
-  }
+//  @Mock
+//  private GatewayEventManager gatewayEventManager;
+//
+//  @Mock
+//  private GatewayActionProducer gatewayActionProducer;
+//
+//  @Test
+//  public void sendCERequestToJobService() throws GatewayException {
+//    // Given
+//    CreateFieldWorkerJobRequest createJobRequest = new FieldWorkerRequestMessageBuilder()
+//        .buildCreateFieldWorkerJobRequestCE();
+//
+//    // When
+//    gatewayActionAdapter.sendJobRequest(createJobRequest, CANONICAL_CE_CREATE_SENT);
+//
+//    // Then
+//    Mockito.verify(gatewayEventManager).triggerEvent(anyString(), eq(CANONICAL_CE_CREATE_SENT));
+//  }
 }
