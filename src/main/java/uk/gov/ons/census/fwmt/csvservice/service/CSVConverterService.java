7bec9fced18d1c9f00711d9563bdd58093edd37a
“JacobHarrisonONS” <jacobharrison@outlook.com>
2021-03-29 11:26:24 +0100

    remove unused import

--- src/main/java/uk/gov/ons/census/fwmt/csvservice/service/CSVConverterService.java
+++ src/main/java/uk/gov/ons/census/fwmt/csvservice/service/CSVConverterService.java
@@ -2,8 +2,6 @@ package uk.gov.ons.census.fwmt.csvservice.service;
 
 import uk.gov.ons.census.fwmt.common.error.GatewayException;
 
-import java.io.IOException;
-
 public interface CSVConverterService {
   void convertToCanonical() throws GatewayException;
 }
