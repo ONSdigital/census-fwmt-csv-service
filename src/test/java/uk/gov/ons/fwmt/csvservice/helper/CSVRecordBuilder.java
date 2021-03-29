e15ebc017d756d29404ff6800e2e56be4c410db0
“JacobHarrisonONS” <jacobharrison@outlook.com>
2021-03-29 14:11:57 +0100

    add test

--- src/test/java/uk/gov/ons/fwmt/csvservice/helper/CSVRecordBuilder.java
+++ src/test/java/uk/gov/ons/fwmt/csvservice/helper/CSVRecordBuilder.java
@@ -1,6 +1,7 @@
 package uk.gov.ons.fwmt.csvservice.helper;
 
 import uk.gov.ons.census.fwmt.csvservice.dto.CCSPropertyListing;
+import uk.gov.ons.census.fwmt.csvservice.dto.CeCreate;
 import uk.gov.ons.census.fwmt.csvservice.dto.NCIntListing;
 
 import java.math.BigDecimal;
@@ -29,5 +30,34 @@ public class CSVRecordBuilder {
     ncIntListing.setLongitude(Double.valueOf(0.11));
 
     return ncIntListing;
+  }
+
+  public CeCreate createCeCreateRecord() {
+    CeCreate ceCreate = new CeCreate();
+
+    ceCreate.setCaseId("e8bfa75f-ebbd-487f-9973-279233ad0887");
+    ceCreate.setCaseReference("2776536191");
+    ceCreate.setUprn("9992776536191");
+    ceCreate.setEstabUprn("9992776536191");
+    ceCreate.setSecureEstablishment(false);
+    ceCreate.setEstablishmentType("CARE HOME");
+    ceCreate.setFieldOfficerId("SH-RNL1-ZF-02");
+    ceCreate.setCoordinatorId("SH-RNL1-ZF");
+    ceCreate.setOrganisationName("Org name");
+    ceCreate.setAddressLine1("Carehome");
+    ceCreate.setAddressLine2("Road");
+    ceCreate.setAddressLine3("Enfield");
+    ceCreate.setTownName("London");
+    ceCreate.setPostCode("EN3 6AY");
+    ceCreate.setOa("E00007186");
+    ceCreate.setLatitude(51.668884);
+    ceCreate.setLongitude(-0.031528);
+    ceCreate.setHandDelivered(true);
+    ceCreate.setCeCE1Complete(true);
+    ceCreate.setCeExpectedCapacity(15);
+    ceCreate.setCeActualResponses(4);
+    ceCreate.setUaa(false);
+
+    return ceCreate;
   }
 }
